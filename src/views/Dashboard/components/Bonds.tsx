6d075f532ff8aa3e1ce2435376990b64ab017e34
darkcheftar <darkcheftar.connect@gmail.com>
2022-03-15 23:35:15 +0530

    populated data in bonds dashboard

--- src/views/Dashboard/components/Bonds.tsx
+++ src/views/Dashboard/components/Bonds.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import useBondStats from '../../../hooks/useBondStats';
+import useTokenBalance from '../../../hooks/useTokenBalance';
+import useBombFinance from '../../../hooks/useBombFinance';
+
+import {getDisplayBalance} from '../../../utils/formatBalance';
+
 const Styleddiv = styled.div`
 height:max-content;
 background: rgba(32, 37, 67, 0.5);
@@ -8,6 +14,9 @@ color: white;
 `
 
 const Bonds: React.FC<any> = () => {
+  const bondStat = useBondStats();
+  const bombFinance = useBombFinance();
+  const bondBalance = useTokenBalance(bombFinance?.BBOND);
 
   return (
    <Styleddiv>
@@ -15,11 +24,11 @@ const Bonds: React.FC<any> = () => {
      <p>BBOND can be purchased only on contraction periods, when TWAP of BOMB is below 1</p>
       <div>
         <div>Current Price: (Bomb)^2</div>
-        <div>BBond = {4523}BTCB</div>
+        <div>10000 BBond = {Number(bondStat?.tokenInFtm).toFixed(4) || '-'} BTCB</div>
       </div>
       <div>
-        <div>Current Price: (Bomb)^2</div>
-        <div>BBond = {4523}BTCB</div>
+        <div>Available to Redeem</div>
+        <div>{getDisplayBalance(bondBalance)}</div>
       </div>
       <div>
         <div>Purchase BBond <button>Purchase</button></div>
