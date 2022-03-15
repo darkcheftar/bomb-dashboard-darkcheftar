ffc2845465ac0b44aafc0955c51d9ef8175b642a
darkcheftar <darkcheftar.connect@gmail.com>
2022-03-16 00:30:13 +0530

    populated known stats in dashboard's Boardroom

--- src/views/Dashboard/components/Boardroom.tsx
+++ src/views/Dashboard/components/Boardroom.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import Stats from './Stats';
 
+import useStakedBalanceOnBoardroom from '../../../hooks/useStakedBalanceOnBoardroom';
+import useTotalStakedOnBoardroom from '../../../hooks/useTotalStakedOnBoardroom';
+import useEarningsOnBoardroom from '../../../hooks/useEarningsOnBoardroom';
+
+import { getDisplayBalance } from '../../../utils/formatBalance';
+
 const Boardroom: React.FC<any> = () => {
-  const icon = '';
-  const totalstaked = 0;
-  const yourstake = 0;
-  const returns = 0;
-  const earned = 0;
-  const tvl = 0;
+
   const deposit = () => {};
   const withdraw = () => {};
   const claimrewards = () => {};
+  const stakedBalance = useStakedBalanceOnBoardroom();
+  const totalStaked = useTotalStakedOnBoardroom();
+  const earning = useEarningsOnBoardroom();
+
   const values = {
+   
     heading:'Boardroom',
-    icon,
+    icon:'',
     message:'Stake BSHARE and earn BOMB every epoch',
     bg:true,
-    tvl,
-    totalstaked,
-    yourstake,
-    returns,
-    earned,
+    tvl:0,
+    totalstaked:totalStaked,
+    yourstake:getDisplayBalance(stakedBalance),
+    returns:0,
+    earned:getDisplayBalance(earning),
     deposit,
     withdraw,
     claimrewards,
