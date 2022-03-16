13a14f0d29c2566ca21c0cf825b9339d5d6bef02
darkcheftar <darkcheftar.connect@gmail.com>
2022-03-16 10:36:39 +0530

    added daily return to Dashboard Boardroom

--- src/views/Dashboard/components/Boardroom.tsx
+++ src/views/Dashboard/components/Boardroom.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Stats from './Stats';
 
 import useStakedBalanceOnBoardroom from '../../../hooks/useStakedBalanceOnBoardroom';
 import useTotalStakedOnBoardroom from '../../../hooks/useTotalStakedOnBoardroom';
 import useEarningsOnBoardroom from '../../../hooks/useEarningsOnBoardroom';
+import useBombFinance from '../../../hooks/useBombFinance';
 
 import { getDisplayBalance } from '../../../utils/formatBalance';
+import useFetchBoardroomAPR from '../../../hooks/useFetchBoardroomAPR';
 
 const Boardroom: React.FC<any> = () => {
-
+  const bombFinanace = useBombFinance();
+  const [boardroomTVL, setBoardroomTVL] = useState(0);
+  useEffect(()=>{
+    const TVLcalc = async ()=>{
+      const BSHAREPrice = (await bombFinanace.getShareStat()).priceInDollars;
+      const boardroomtShareBalanceOf = await bombFinanace.BSHARE.balanceOf(bombFinanace.currentBoardroom().address);
+      setBoardroomTVL(Number(getDisplayBalance(boardroomtShareBalanceOf, bombFinanace.BSHARE.decimal)) * Number(BSHAREPrice));
+    }
+    TVLcalc();
+    },
+    [bombFinanace]
+  );
   const deposit = () => {};
   const withdraw = () => {};
   const claimrewards = () => {};
@@ -22,10 +35,10 @@ const Boardroom: React.FC<any> = () => {
     icon:'',
     message:'Stake BSHARE and earn BOMB every epoch',
     bg:true,
-    tvl:0,
+    tvl:boardroomTVL,
     totalstaked:getDisplayBalance(totalStaked),
     yourstake:getDisplayBalance(stakedBalance),
-    returns:0,
+    returns:useFetchBoardroomAPR()/365,
     earned:getDisplayBalance(earning),
     deposit,
     withdraw,
